9895b14ad6104b7913403a94bf00128a2b017325
diego cantero <diegomarianocantero@gmail.com>
2013-08-20 13:42:51 -0300

    eliminacion de carpeta obsoleta

--- Integrador/Compralibros.com/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-#Cerramos toda la sesion y destruimos las variables de sesion	
-session_unset();
-session_destroy();
-header('location: index.php');
-?>
--- Integrador/Compralibros.com/vaciarcarrito.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-unset($_SESSION['carrito']);
-$_SESSION['importe'] = 0;
-
-header('location:vercarrito.php');
-
-?>
